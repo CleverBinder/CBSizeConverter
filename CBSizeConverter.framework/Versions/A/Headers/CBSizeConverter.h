74b7ecc5ca7f77f64939c9cb671c4b13a2daef80
morten jeppesen <mkj@cleverbinder.com>
2016-02-19 15:09:05 +0100

    first commit

---
+++ CBSizeConverter.framework/Versions/A/Headers/CBSizeConverter.h
@@ -0,0 +1,16 @@
+//
+//  CBSizeConverter.h
+//  CBSizeConverter
+//
+//  Created by Morten Jeppesen on 19/02/16.
+//  Copyright © 2016 CleverBinder A/S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//
+
+@interface CBSizeConverter : NSObject
+
+-(id)start;
+-(int)getFromInch:(int)inch;
+@end
